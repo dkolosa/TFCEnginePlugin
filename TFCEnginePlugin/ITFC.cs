08e3b9b7c0d043a7a5a321e014167eb6c9282f6a
Daniel Kolosa <daniel.s.kolosa@wmich.edu>
2016-08-25 11:26:32 -0400

    initial Commit

---
+++ TFCEnginePlugin/ITFC.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFCEnginePlugin
+{
+    public interface ITFC
+    {
+        string Name { get; set; }
+        double Alpha0 { get; set; }
+        double Alpha1 { get; set; }
+        double Alpha2 { get; set; }
+        double Alpha3 { get; set; }
+        double Alpha4 { get; set; }
+        double Alpha5 { get; set; }
+        double Alpha6 { get; set; }
+        double Alpha7 { get; set; }
+        double Alpha8 { get; set; }
+        double Alpha9 { get; set; }
+        double Alpha10 { get; set; }
+        double Alpha11 { get; set; }
+        double Alpha12 { get; set; }
+        double Alpha13 { get; set; }
+        double Isp { get; set; }
+    }
+}

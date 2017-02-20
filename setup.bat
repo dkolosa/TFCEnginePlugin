34fb5d2d24e06496566c5e32c22341e233323e9f
dan <contact@dkolosa.com>
2017-02-19 20:14:32 -0500

    Added a script to register plugins

---
+++ setup.bat
@@ -0,0 +1,5 @@
+:: This file will register the necessary xml and dll files
+
+regserver32 "User Variables\JScript.TFCEngineValues.EOMFunc.wsc"
+
+regserver32 "TFCEnginePlugin\bin\Debug\TFCEnginePlugin.dll"
